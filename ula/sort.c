5c6a0e474f08f23f6314d2bfdb2964c244eccc17
Celtenn <enescelten@gmail.com>
2025-01-07 21:01:10 +0300

    Add files via upload

--- ula/sort.c
+++ ula/sort.c
@@ -2,9 +2,9 @@
 
 void	temporary_sort(t_node *head)
 {
-	t_node *current;
-	t_node *next_node;
-	int temp;
+	t_node	*current;
+	t_node	*next_node;
+	int		temp;
 
 	if (head == NULL && head->next == NULL)
 		return ;
@@ -41,10 +41,10 @@ void	three_numbers(t_stack *three)
 void	three_numbers_case_stack_a(t_stack *three, int a, int b, int c)
 {
 	if (!three || !three->a || !three->a->next || !three->a->next->next)
-		return;
+		return ;
 	if (a > b && a < c && b < c)
 		swap_a(three);
-	else if (a > b && a > c && b > c) 
+	else if (a > b && a > c && b > c)
 	{
 		swap_a(three);
 		reverse_rotate_a(three);
@@ -60,13 +60,13 @@ void	three_numbers_case_stack_a(t_stack *three, int a, int b, int c)
 		reverse_rotate_a(three);
 }
 
-int sort(t_stack *stack, int size) 
+int	sort(t_stack *stack, int size)
 {
 	if (!stack || !stack->a)
-		return 0;
-		stack->size_a = list_size(stack->a);
-		stack->size_b = 0;
-	if (check_sorted(stack->a, 0) == 0) 
+		return (0);
+	stack->size_a = list_size(stack->a);
+	stack->size_b = 0;
+	if (check_sorted(stack->a, 0) == 0)
 	{
 		if (size == 2)
 			swap_a(stack);
@@ -75,19 +75,17 @@ int sort(t_stack *stack, int size)
 		else
 			quicksort_stack_a(stack, size, 0);
 	}
-	return 0;
+	return (0);
 }
 
-int ft_push(t_stack *stack, int len, int push) 
+int	ft_push(t_stack *stack, int len, int push)
 {
 	if (!stack || len <= 0)
-		return len;
-
+		return (len);
 	if (push == 0)
 		push_b(stack);
 	else
 		push_a(stack);
-
 	len--;
-	return len;
+	return (len);
 }
